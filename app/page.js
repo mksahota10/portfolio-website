8e9909486d2c4f5c1d4a5ba004f9bd7330ca7828
User <user@Users-MacBook-Pro.local>
2023-12-19 14:02:56 -0800

    Navbar completed

--- app/page.js
+++ app/page.js
@@ -1,11 +1,18 @@
 import Image from 'next/image'
 import HeroSection from './components/HeroSection'
+import NavBar from './components/NavBar'
+import MenuOverLay from './components/MenuOverLay'
+
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212]">
-      <div class="container mx-auto px-12 py-4">
+      <NavBar />
+      
+      <div class="container mt-24 mx-auto px-12 py-4">
       <HeroSection />
+      
+
       </div>
     </main>
   )
