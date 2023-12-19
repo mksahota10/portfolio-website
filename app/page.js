321a979732b621bd93cb13f72bb4f991e3787830
User <user@Users-MacBook-Pro.local>
2023-12-19 15:33:44 -0800

    aboutme section completed

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 import Image from 'next/image'
 import HeroSection from './components/HeroSection'
 import NavBar from './components/NavBar'
-import MenuOverLay from './components/MenuOverLay'
+import AboutSection from './components/AboutSection'
 
 
 export default function Home() {
@@ -11,6 +11,7 @@ export default function Home() {
       
       <div class="container mt-24 mx-auto px-12 py-4">
       <HeroSection />
+      <AboutSection />
       
 
       </div>
