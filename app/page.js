cf2ea054cc7c457aa8ab03f4542d31352f3923ca
User <user@Users-MacBook-Pro.local>
2023-12-20 13:03:11 -0800

    project section completed

--- app/page.js
+++ app/page.js
@@ -2,6 +2,7 @@ import Image from 'next/image'
 import HeroSection from './components/HeroSection'
 import NavBar from './components/NavBar'
 import AboutSection from './components/AboutSection'
+import ProjectSection from "./components/ProjectSection";
 
 
 export default function Home() {
@@ -12,6 +13,7 @@ export default function Home() {
       <div class="container mt-24 mx-auto px-12 py-4">
       <HeroSection />
       <AboutSection />
+      <ProjectSection />
       
 
       </div>
