addaea2c249853a6042edf37f7f671a9a25f35c8
User <user@Users-MacBook-Pro.local>
2024-03-02 14:25:26 -0800

    updates to projects

--- app/components/AboutSection.jsx
+++ app/components/AboutSection.jsx
@@ -9,12 +9,13 @@ const TAB_DATA = [
     id: "skills",
     content: (
       <ul className="list-disc pl-2">
+         <li>JavaScript</li>
         <li>Node.js</li>
         <li>Express</li>
-        <li>PostgreSQL</li>
-        <li>Sequelize</li>
-        <li>JavaScript</li>
         <li>React</li>
+        <li>PostgreSQL</li>
+        <li>Python</li>
+        <li>SQL</li>
       </ul>
     ),
   },
@@ -23,7 +24,6 @@ const TAB_DATA = [
     id: "education",
     content: (
       <ul className="list-disc pl-2">
-        <li>Fullstack Software Engineer, General Assembly</li>
         <li> Economics and Communication Studies, San Jose State University</li>
       </ul>
     ),
@@ -33,8 +33,9 @@ const TAB_DATA = [
     id: "certifications",
     content: (
       <ul className="list-disc pl-2">
+        <li>Fullstack Software Engineer, General Assembly</li>
         <li>AWS Cloud Practitioner</li>
-        <li>Google Professional Cloud Developer</li>
+        <li>FINRA Series 7 and 63</li>
       </ul>
     ),
   },
@@ -57,12 +58,7 @@ const AboutSection = () => {
         <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
           <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
           <p className="text-base lg:text-lg">
-            I am a full stack web developer with a passion for creating
-            interactive and responsive web applications. I have experience
-            working with JavaScript, React, Redux, Node.js, Express, PostgreSQL,
-            Sequelize, HTML, CSS, and Git. I am a quick learner and I am always
-            looking to expand my knowledge and skill set. I am a team player and
-            I am excited to work with others to create amazing applications.
+          Results-oriented and forward-thinking professional with a proven track record in optimizing product functionality, driving user engagement, and delivering tangible outcomes. Adept at problem-solving with a proactive approach, I excel in strategic collaboration and leverage cutting-edge technologies to propel product success.
           </p>
           <div className="flex flex-row justify-start mt-8">
             <TabButton
--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -16,7 +16,7 @@ const HeroSection = () => {
           className="col-span-8 place-self-center text-center sm:text-left justify-self-start"
         >
           <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-normal font-extrabold">
-            <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-primary-400 to-pink-600">
               Hello, I&apos;m{" "}
             </span>
             <br></br>
@@ -24,11 +24,11 @@ const HeroSection = () => {
               sequence={[
                 "Preet",
                 1000,
-                "Web Developer",
+                "A Developer",
                 1000,
-                "Developer Advocate",
+                "An Avid Learner",
                 1000,
-                "Avid Learner",
+                "Always Curious",
                 1000,
               ]}
               wrapper="span"
@@ -37,21 +37,20 @@ const HeroSection = () => {
             />
           </h1>
           <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
-            voluptuous.
+           Let's Connect!
           </p>
           <div>
             <Link
               href="/#contact"
               className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-white"
             >
-              Hire Me
+              Find Me Here
             </Link>
             <Link
-              href="/#contact"
+              href="https://docs.google.com/document/d/e/2PACX-1vRdG8QIjqRMMqoJY11MM7fX3y5MmOUBb1rXFKXqKzgreU3RLW0-1EGdMZQN5AMMHw/pub"
               className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-white"
             >
-              DownLoad CV
+              Download My CV
             </Link>
           </div>
         </motion.div>
@@ -61,9 +60,9 @@ const HeroSection = () => {
           transition={{ duration: 0.5 }}
           className="col-span-4 place-self-center mt-4 lg:mt-0"
         >
-          <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
+          <div className=" bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
             <Image
-              src="/images/hero-image.png"
+              src="/images/Pic.jpeg"
               alt="hero image"
               className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
               width={300}
